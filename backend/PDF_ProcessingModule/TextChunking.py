a164d8e23842be4def831b9187b844942364cdba
ThakarSairaj <178469889+ThakarSairaj@users.noreply.github.com>
2025-09-16 00:26:02 +0530

    chunk length chunking

---
+++ backend/PDF_ProcessingModule/TextChunking.py
@@ -0,0 +1,23 @@
+import PDFExtraction as pd
+import TextCleaning as tc
+
+def text_chunks(text, chunks = 1000, overlap = 100):
+
+    chun = []
+    words = text.split()
+
+    for i in range(0, len(words), chunks - overlap):
+        chunk = ' '.join(words[i:i + chunks])
+        if(len(chunk.strip()) > 50):
+            chun.append(chunk.strip())
+        
+    return chun
+
+
+raw = pd.pdf_extract('Brain_Facts_BookHighRes.pdf')
+
+clean = tc.text_clean(raw)
+
+chunkss = text_chunks(clean)
+
+print(clean)
